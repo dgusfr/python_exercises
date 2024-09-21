5a5cdb8927fee62e5c881976ba2dde48a3ab687a
dgusfr <diegogustavo.fr@gmail.com>
2024-09-21 15:25:09 -0300

    feat(tupla): coordenada

--- Tipos de Dados/tupla_coordenada.py
+++ Tipos de Dados/tupla_coordenada.py
@@ -1,4 +1,11 @@
-'''Crie uma tupla com 4 coordenadas (números de ponto flutuante) e mostre o valor do segundo item da tupla.'''
+'''Crie uma tupla com 4 coordenadas (números de ponto flutuante) e mostre o valor do segundo item da tupla.'''.encode
+import math
 
-coordenada = (10.5, 22.3, 61.3, 1.456)
-print(coordenada[1])
+coordenada = [(10.5, 22.3), (61.3, 1.456), (12.1, 42.4)]
+print(coordenada)
+
+ponto_um = pow((coordenada[2] - coordenada[0]), 2)
+ponto_dois = pow((coordenada[3] - coordenada[1]), 2)
+
+distancia = math.sqrt(ponto_um + ponto_dois)
+print(distancia)
