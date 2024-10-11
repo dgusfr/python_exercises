7654b159917e07085f04b454b0da5f424d9d996b
dgusfr <diegogustavo.fr@gmail.com>
2024-10-11 08:35:34 -0300

    feat(traffic)

---
+++ Condicionais/controle_de_trafego.py
@@ -0,0 +1,4 @@
+numero_de_veiculos = int(input("Quantidade de veiculos que passaram:"))
+limite_de_trafego = int(input("Digite o limite de veiculos que podem trafegar:"))
+intervalo_de_tempo = float(input("Digite o intervalo de tempo (em horas):"))
+media_de_veiculos = 0
