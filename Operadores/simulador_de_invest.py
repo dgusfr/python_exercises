9b107796918be16ebdac88b893f8973bb2e13ba6
dgusfr <diegogustavo.fr@gmail.com>
2024-10-11 09:13:41 -0300

    feat(iinvest)

--- Operadores/simulador_de_invest.py
+++ Operadores/simulador_de_invest.py
@@ -1,18 +1,20 @@
 sair = True
-capitalizacao = input("O periodo de capitalização será em meses ou anos? (Digite: mes ou anos ou sair)")
+capitalizacao = input("O periodo de capitalização será em meses ou anos? (Digite: mes ou anos ou sair):\n")
 
 while sair == True:
-  if capitalizacao.lower() == 'mês':
-    valor_presente = float(input("Digiote o valor presente a ser aplicado:"))
+  if capitalizacao.lower() == 'mes':
+    valor_presente = float(input("Digite o valor presente a ser aplicado:"))
     taxa = float(input("Digite a taxa a ser aplicada:"))
-    tempo = int(input("Digite o tempo que será aplicado o valor (Em meses)"))
+    tempo = int(input("Digite o tempo que será aplicado o valor (Em meses):"))
     valor_final = (valor_presente * ((1 + (taxa/ 12)) ** ( 12 * tempo )))
+    print(f"O valor final será de: {valor_final} ")
   elif capitalizacao.lower() == 'anos':
-    valor_presente = float(input("Digiote o valor presente a ser aplicado:"))
+    valor_presente = float(input("Digite o valor presente a ser aplicado:"))
     taxa = float(input("Digite a taxa a ser aplicada:"))
-    tempo = int(input("Digite o tempo que será aplicado o valor (Em meses)"))
+    tempo = int(input("Digite o tempo que será aplicado o valor (Em meses):"))
     valor_final = (valor_presente * ((1 + (taxa/ 1)) ** ( 1 * tempo )))
+    print(f"O valor final será de: {valor_final} ")
   else:
     sair == False
 
-    
+
