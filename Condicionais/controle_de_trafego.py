56a066ec3043a23fa4a9a89ac2feda0bd46c1efc
dgusfr <diegogustavo.fr@gmail.com>
2024-10-11 08:37:25 -0300

    feat(traffics): final

--- Condicionais/controle_de_trafego.py
+++ Condicionais/controle_de_trafego.py
@@ -2,3 +2,11 @@ numero_de_veiculos = int(input("Quantidade de veiculos que passaram:"))
 limite_de_trafego = int(input("Digite o limite de veiculos que podem trafegar:"))
 intervalo_de_tempo = float(input("Digite o intervalo de tempo (em horas):"))
 media_de_veiculos = 0
+
+if numero_de_veiculos > limite_de_trafego:
+  print("Trafego alto")
+else:
+  print("Trafego normal")
+  
+media_de_veiculos = numero_de_veiculos / intervalo_de_tempo
+print(f"A media de veiculos por hora é:{media_de_veiculos}")
