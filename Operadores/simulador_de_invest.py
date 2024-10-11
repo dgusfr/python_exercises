463e6fc41dbab90c7306f7586478a350281c789f
dgusfr <diegogustavo.fr@gmail.com>
2024-10-11 08:56:58 -0300

    feat

--- Operadores/simulador_de_invest.py
+++ Operadores/simulador_de_invest.py
@@ -1,12 +1,18 @@
-
-taxa = float(input("Digite a taxa a ser aplicada:"))
-numero_de_capitalizacao = 0
-tempo = 0
-
+sair = True
 capitalizacao = input("O periodo de capitalização será em meses ou anos? (Digite: mes ou anos ou sair)")
 
-if capitalizacao.lower() == 'mês':
-  valor_presente = float(input("Digiote o valor presente a ser aplicado:"))
-  taxa = float(input("Digite a taxa a ser aplicada:"))
-  tempo = int(input("Digite o tempo que será aplicado o valor (Em meses)"))
-  valor_final = (valor_presente * ((1 + (taxa/ 12)) ** ( 12 * tempo )))
+while sair == True:
+  if capitalizacao.lower() == 'mês':
+    valor_presente = float(input("Digiote o valor presente a ser aplicado:"))
+    taxa = float(input("Digite a taxa a ser aplicada:"))
+    tempo = int(input("Digite o tempo que será aplicado o valor (Em meses)"))
+    valor_final = (valor_presente * ((1 + (taxa/ 12)) ** ( 12 * tempo )))
+  elif capitalizacao.lower() == 'anos':
+    valor_presente = float(input("Digiote o valor presente a ser aplicado:"))
+    taxa = float(input("Digite a taxa a ser aplicada:"))
+    tempo = int(input("Digite o tempo que será aplicado o valor (Em meses)"))
+    valor_final = (valor_presente * ((1 + (taxa/ 1)) ** ( 1 * tempo )))
+  else:
+    sair == False
+
+    
