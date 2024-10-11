901d22b5b2ca8dbe1002fc28b61a2da270d94b70
dgusfr <diegogustavo.fr@gmail.com>
2024-10-11 08:45:04 -0300

    feat(struct): desct

--- Condicionais/sistema_de_descontos.py
+++ Condicionais/sistema_de_descontos.py
@@ -1,10 +1,11 @@
 valor_total_compra = float(input("Digite o valor total da compra:"))
-valor_com_desconto = 0
 
 if valor_total_compra >= 1000:
   valor_com_desconto = valor_total_compra - (valor_total_compra * 0.1)
+  print(f"O valor com desconto é {valor_com_desconto}")
 elif 500 <= valor_total_compra < 1000:
   valor_com_desconto = valor_total_compra - (valor_total_compra * 0.05)
+  print(f"O valor com desconto é {valor_com_desconto}")
 else:
   print("Sem descontos aplicáveis")
   
