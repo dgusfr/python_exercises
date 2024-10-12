8f47b947014c960ec08fd76a267e1f2430354a48
dgusfr <diegogustavo.fr@gmail.com>
2024-10-12 09:54:36 -0300

    feat(agendamento)

--- Bibliotecas/agendamento_date.py
+++ Bibliotecas/agendamento_date.py
@@ -1,5 +1,11 @@
-from datetime import date
+from datetime import datetime
 
-data_do_compromisso = date.strptime(input("Digite a data e hora (formato: DD/MM/AAAA HH:MM):"))   
-data_de_hoje = date.now()
+data_do_compromisso = datetime.strptime(input("Digite a data e hora (formato: DD/MM/AAAA HH:MM): "), "%d/%m/%Y %H:%M")
 
+data_de_hoje = datetime.now()
+
+if data_do_compromisso < data_de_hoje:
+    print("Erro: O compromisso está marcado para uma data anterior à atual.")
+else:
+    tempo_restante = data_do_compromisso - data_de_hoje
+    print(f"Faltam {tempo_restante} para o compromisso.")
