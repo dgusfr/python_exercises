d8b2fbfb25dc169ea862dc8b92e3c518a2c40de7
dgusfr <diegogustavo.fr@gmail.com>
2025-06-17 20:40:54 -0300

    Fix output formatting for age calculation in ano_cem_anos.py

--- Lista_1/ano_cem_anos.py
+++ Lista_1/ano_cem_anos.py
@@ -2,4 +2,4 @@ nome_completo = input("Digite seu nome completo:  ")
 idade = int(input("Digite sua idade: "))
 ano_atual = 2025
 
-print(f"Olá {nome_completo}, você vai completar 100 anos em")
+print(f"Olá {nome_completo}, você vai completar 100 anos em {ano_atual + (100 - idade)}")
