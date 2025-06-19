7ecfb8da4a23d917facab162884b0f2f46153308
dgusfr <diegogustavo.fr@gmail.com>
2025-06-19 11:44:15 -0300

    Add user input for age and define classificar_idade function

--- Lista_1/classificacao_idade_funcao.py
+++ Lista_1/classificacao_idade_funcao.py
@@ -2,3 +2,7 @@
 Crie uma função classificar_idade() que recebe a idade como parâmetro e retorna se a pessoa é "criança", "adolescente", "adulto" ou "idoso". Utilize essa função com dados lidos do usuário.
 """
 
+idade = int(input("Digite sua idade:"))
+
+def classificar_idade(idade):
+  if idade < 12:
