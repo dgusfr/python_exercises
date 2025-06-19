48771032c0cd6dfe03756779c5f9d65adb37fca5
dgusfr <diegogustavo.fr@gmail.com>
2025-06-19 11:43:20 -0300

    Add initial implementation of classificar_idade function to classify age groups

---
+++ Lista_1/classificacao_idade_funcao.py
@@ -0,0 +1,4 @@
+"""
+Crie uma função classificar_idade() que recebe a idade como parâmetro e retorna se a pessoa é "criança", "adolescente", "adulto" ou "idoso". Utilize essa função com dados lidos do usuário.
+"""
+
