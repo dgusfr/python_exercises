c380fc75061a8147102a1bc369ed79f6f00e2c4d
dgusfr <diegogustavo.fr@gmail.com>
2025-06-19 12:41:09 -0300

    Refactor dictionary initialization for book data in dicionario_dados_livro.py for improved readability

--- Lista_1/dicionario_dados_livro.py
+++ Lista_1/dicionario_dados_livro.py
@@ -2,12 +2,7 @@
 Crie um dicionário para armazenar dados de um livro: título, autor, ano e número de páginas. Solicite os dados ao usuário e, ao final, exiba o dicionário formatado.
 """
 
-livro = {
-  "titulo" :"",
-  "autor": "",
-  "ano": 0,
-  "numero_paginas": 0
-}
+livro = {"titulo": "", "autor": "", "ano": 0, "numero_paginas": 0}
 
 livro["titulo"] = input("Digite o titulo do livro: ")
 livro["autor"] = input("Digite o autor do livro: ")
@@ -19,8 +14,5 @@ print("Informações do livro:")
 print("____________________")
 print(f"Título: {livro['titulo']}")
 print(f"Autor: {livro['autor']}")
-print(f"Ano: {livro['ano']}") 
+print(f"Ano: {livro['ano']}")
 print(f"Número de Páginas: {livro['numero_paginas']}")
-
-
-
