dfd73db75d92cef4cc50b6ac16a5e2bf1d32939f
dgusfr <diegogustavo.fr@gmail.com>
2025-06-19 11:51:52 -0300

    Refactor classificar_idade function to improve readability and ensure proper indentation

--- Lista_1/classificacao_idade_funcao.py
+++ Lista_1/classificacao_idade_funcao.py
@@ -1,8 +1,14 @@
-"""
-Crie uma função classificar_idade() que recebe a idade como parâmetro e retorna se a pessoa é "criança", "adolescente", "adulto" ou "idoso". Utilize essa função com dados lidos do usuário.
-"""
-
 idade = int(input("Digite sua idade:"))
 
 def classificar_idade(idade):
   if idade < 12:
+    print("Você é uma criança.")
+  elif 12 <= idade < 17:
+    print("Você é um adolecente.")
+  elif 17 <= idade < 60:
+    print("Você é um adulto.")
+  else:
+    print("Você é um idoso.")
+
+
+classificar_idade(idade)
