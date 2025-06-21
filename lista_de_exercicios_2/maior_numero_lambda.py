635d38167ffaaa4d3236098489811e273e6e5be6
dgusfr <diegogustavo.fr@gmail.com>
2025-06-21 11:33:38 -0300

    Refactor comments in maior_numero_lambda.py for clarity

--- lista_de_exercicios_2/maior_numero_lambda.py
+++ lista_de_exercicios_2/maior_numero_lambda.py
@@ -3,7 +3,7 @@ Implemente uma função lambda que receba dois números e retorne o maior deles.
 """
 
 maior_numero = lambda x, y: x if x > y else y
-# Testando a função lambda com diferentes pares de valores
-print(maior_numero(10, 20))  # Deve retornar 20
-print(maior_numero(30, 15))  # Deve retornar 30
-print(maior_numero(5, 5))  # Deve retornar 5
+
+print(maior_numero(10, 20))
+print(maior_numero(30, 15))
+print(maior_numero(5, 5))
