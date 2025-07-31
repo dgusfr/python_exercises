861df8e26f2bcbdc8eca1e528017e34ec7c13093
dgusfr <diegogustavo.fr@gmail.com>
2025-07-30 21:18:03 -0300

    feat update

--- LinkedList/main.py
+++
@@ -1,9 +0,0 @@
-from linked_list import LinkedList
-
-if __name__ == "__main__":
-    lista = LinkedList()
-    lista.insere_final_lista(8)
-    lista.insere_final_lista(6)
-    lista.tamanho_lista()
-    print(lista.index(6))
-    print(lista[1])
