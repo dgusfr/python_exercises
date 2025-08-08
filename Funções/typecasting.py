cfa45883dc88d590949a7c26df4203e3d43bfe3c
dgusfr <diegogustavo.fr@gmail.com>
2025-08-08 17:00:41 -0300

    feat(add)

--- Funções/typecasting.py
+++ Funções/typecasting.py
@@ -4,7 +4,18 @@ telefones = ["11987654321", "21912345678", "31987654321", "11911223344"]
 def converte_para_inteiro(lista_telefones):
     telefones_convertidos = []
 
-    for telefone in telefones:
+    for telefone in lista_telefones:
         telefones_convertidos.append(int(telefone))
 
-    return telefones_convertidos
+    verifica_tipo(telefones_convertidos)
+
+
+def verifica_tipo(lista_telefones):
+    for telefone in lista_telefones:
+        if isinstance(telefone, int):
+            print(f"✓ Telefone {telefone} é um inteiro válido")
+        else:
+            print(f"✗ Erro: {telefone} não é um inteiro")
+
+
+converte_para_inteiro(telefones)
