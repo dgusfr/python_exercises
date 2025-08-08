20f5afa6d2f7ca8b406accffebfad4024de5bb5d
dgusfr <diegogustavo.fr@gmail.com>
2025-08-08 19:40:34 -0300

    feat add

--- Funções/recursividade.py
+++ Funções/recursividade.py
@@ -1,8 +1,17 @@
 def soma_ate_n(n):
-    if n == 0:
-        return 1
+    if n <= 1:
+        return n
     else:
         return n + soma_ate_n(n - 1)
 
 
-print(soma_ate_n(5))
+print(soma_ate_n(5))  # 5 + 4 + 3 + 2 + 1 = 15
+
+"""
+soma_recursiva(5)
+↳ return 5 + soma_recursiva(4)
+             ↳ return 4 + soma_recursiva(3)
+                          ↳ return 3 + soma_recursiva(2)
+                                       ↳ return 2 + soma_recursiva(1)
+                                                    ↳ return 1  // <-- CASO BASE ATINGIDO
+"""
