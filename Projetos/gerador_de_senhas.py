74b173a817df484c2b7bfc7765cc9a8e608538ad
dgusfr <diegogustavo.fr@gmail.com>
2025-08-10 13:54:49 -0300

    feat add

---
+++ Projetos/gerador_de_senhas.py
@@ -0,0 +1,6 @@
+"""
+Crie um código em Python que gere uma senha aleatória de 12 caracteres, contendo pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial, e exiba a senha gerada.
+
+Exemplo de saída:
+Senha gerada: A1b@C3d$E5f&
+"""
