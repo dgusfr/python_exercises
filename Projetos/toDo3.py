8c046efad04bb8b4c6ffbe65ec40a5b7953107a5
dgusfr <diegogustavo.fr@gmail.com>
2025-08-10 14:14:30 -0300

    feat add

---
+++ Projetos/toDo3.py
@@ -0,0 +1,23 @@
+"""
+Crie um código em Python que solicite ao usuário o valor do saque, verifique se é um número válido e múltiplo de 2, e calcule a menor quantidade de cédulas (R$ 100, 50, 20, 10, 5, 2) necessárias. Exiba as cédulas entregues ou uma mensagem de erro apropriada (para valor inválido ou não múltiplo de 2).
+
+Exemplo de entrada válida:
+Digite o valor do saque: 188
+Saída:
+Cédulas entregues:
+1 de R$ 100
+1 de R$ 50
+1 de R$ 20
+1 de R$ 10
+1 de R$ 5
+1 de R$ 2
+
+Exemplo de valor inválido (ímpar):
+Erro: O valor deve ser múltiplo de 2.
+
+Exemplo de entrada inválida (não numérica):
+Tratamento de erro apropriado.
+
+---
+
+"""
