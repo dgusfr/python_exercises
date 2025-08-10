87247a3a9f6d96f3245db68fcd5bb390f4f16cb5
dgusfr <diegogustavo.fr@gmail.com>
2025-08-10 14:13:26 -0300

    feat add

--- Projetos/toDo2.py
+++ Projetos/toDo2.py
@@ -29,6 +29,22 @@ adivinhar_numero()
 
 ___
 
+Crie um codigo em python que gere um número aleatório entre 1 e 100, permita que o usuário tente adivinhá-lo, informando se o palpite é maior ou menor, até acertar. Trate erros de entrada: se o valor for inválido (não numérico) ou fora do intervalo 1-100, lance uma exceção ValueError e exiba uma mensagem apropriada.
 
+Exemplo de acerto (supondo número 37):
+Tente adivinhar o número (1-100): 37
+Saída: Parabéns! Você acertou o número 37.
+
+Exemplo de palpite baixo:
+Muito baixo! Tente novamente.
+
+Exemplo de palpite alto:
+Muito alto! Tente novamente.
+
+Exemplo de entrada inválida (fora do intervalo):
+Entrada inválida: Número fora do intervalo! Digite um número entre 1 e 100.
+
+Exemplo de entrada inválida (não numérica):
+Entrada inválida: invalid literal for int() with base 10: 'abc'
 
 """
