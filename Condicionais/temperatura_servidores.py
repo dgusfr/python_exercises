92411d8f5789e8d675efbfb7ed15901329c3e388
dgusfr <diegogustavo.fr@gmail.com>
2025-11-26 13:04:53 -0300

    feat: add temperature monitoring script

---
+++ Condicionais/temperatura_servidores.py
@@ -0,0 +1,6 @@
+temperatura_atual = float(input("Digite a temperatura atual:"))
+
+if temperatura_atual > 25.0:
+    print("Alerta! temperatura acima do limite permitido")
+else:
+    print("Temperatura no limite permitido")
