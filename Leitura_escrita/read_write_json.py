89fd9d6b405a4640585e046437cbd55f2bc20313
dgusfr <diegogustavo.fr@gmail.com>
2025-11-30 13:39:32 -0300

    feat(refact): josn file

--- Leitura_escrita/read_write_json.py
+++ Leitura_escrita/read_write_json.py
@@ -1,6 +1,9 @@
 import json
 
-dados = {"nome": "Ana", "idade": 32, "enderecos": ["Endereço A", "Endereço B"]}
+dados = {"nome": "Ana", "idade": 32, "enderecos": ["Endereco A", "Endereco B"]}
+
+with open("dados.json", "w") as f:
+    json.dump(dados, f)
 
 with open("dados.json", "r") as f:
     dados_lidos = json.load(f)
